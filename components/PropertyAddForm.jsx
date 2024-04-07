a0509b44c1e13dd97d7bdf18476fc4b556309f79
Mickey Puri <mickeypuri@hotmail.com>
2024-04-07 17:48:51 +0100

    Add Property POST API route

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -90,7 +90,11 @@ const PropertyAddForm = () => {
   }
   
   return (
-    <form>
+    <form 
+      action="/api/properties" 
+      method="POST"
+      encType="multipart/form-data"
+    >
       <h2 className="text-3xl text-center font-semibold mb-6">
         Add Property
       </h2>
@@ -483,7 +487,7 @@ const PropertyAddForm = () => {
         <input
           type="text"
           id="seller_name"
-          name="seller_info.name."
+          name="seller_info.name"
           className="border rounded w-full py-2 px-3"
           placeholder="Name"
           value={fields.seller_info.name}
@@ -535,6 +539,7 @@ const PropertyAddForm = () => {
           className="border rounded w-full py-2 px-3"
           accept="image/*"
           multiple
+          required
           onChange={handleImageChange}
         />
       </div>
