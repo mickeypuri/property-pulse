3b20ac7c69d96d425b5889b591902b43701a3551
Mickey Puri <mickeypuri@hotmail.com>
2024-04-21 13:14:52 +0100

    Display Search Results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,7 +1,11 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
@@ -39,9 +43,33 @@ const SearchResultsPage = () => {
   console.log(properties);
 
   return (
-    <div>
-      Search Results Page
-    </div>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8 mb-3">
+          <PropertySearchForm />   
+        </div>
+      </section>
+      {loading ? (<Spinner />) : (
+          <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+              <Link href={"/properties"} className="flex items-center text-blue-500 hover:underline mb-3">
+                <FaArrowAltCircleLeft className="mr-2 mb-1" />
+                 Back To Properties
+              </Link>
+              <h className="text-2xl mb-4">Search Results</h>
+              {properties.length === 0 ? (
+                <p>No search results found</p>
+              ) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+              )}
+            </div>
+          </section>
+        )}
+    </>
   )
 };
 
