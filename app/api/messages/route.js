f19df4bb608b6d7170b469ecef4988f6884bb270
Mickey Puri <mickeypuri@hotmail.com>
2024-04-23 07:04:01 +0100

    Fetch Messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,6 +4,27 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
+export const GET = async (request) => {
+  try {
+    await connectDB();
+    const sessionUser = await getSessionUser();
+    if (!sessionUser?.userId) {
+      return new Response(JSON.stringify({message: "User Id is required"}), { status: 401 });
+    }
+
+    const {userId } = sessionUser;
+
+    const messages = await Message.find({ recipient: userId })
+      .populate("sender", "name")
+      .populate("property", "title");
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500});
+  }
+};
+
 // POST /api/messages
 export const POST = async (request) => {
   try {
