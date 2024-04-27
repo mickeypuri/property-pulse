35a69345ffe39fc97bc404b5bc62dec8d20c3cb1
Mickey Puri <mickeypuri@hotmail.com>
2024-04-27 17:12:09 +0100

    Pagination Component

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -2,12 +2,13 @@
 import { useState, useEffect } from "react";
 import PropertyCard from "@/components/PropertyCard";
 import Spinner from "@/components/Spinner";
+import Pagination from "./Pagination";
 
 const Properties = () => {
   const [properties, setProperties] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(3);
+  const [pageSize, setPageSize] = useState(6);
   const [totalItems, setTotalItems] = useState(0);
 
   useEffect(() => {
@@ -31,7 +32,11 @@ const Properties = () => {
     };
     fetchProperties();
 
-  }, [])
+  }, [page, pageSize])
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
   
   return loading ? (<Spinner />) : (
     <section className="px-4 py-6">
@@ -46,6 +51,12 @@ const Properties = () => {
             </div>
           )}
         </div>
+        <Pagination 
+          page={page} 
+          pageSize={pageSize} 
+          totalItems={totalItems} 
+          onPageChange={handlePageChange}
+        />
       </section>
   )
 };
